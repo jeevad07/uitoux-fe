dd712f813a20f1253128b34ba7d2293038af5d6e
unknown <jeevasiva003@gmail.com>
2024-06-27 02:10:33 +0530

    updated dashboard things

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NgbNavConfig, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgbCarouselModule],
+  imports: [NgbCarouselModule,NgbNavModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  images = ["src/assets/image/slide-1.jpeg", 1011, 984];
+
+  leftarrow='<';
 }
